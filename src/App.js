59e1142dfe09655a8977fa578ce89831f3de9396
shourovahmd <moh.abuhanifa@gmail.com>
2023-02-18 22:56:00 +0600

    @mui/icons-material

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import { Button, Stack } from "@mui/material";
+import "./App.css";
 
 function App() {
   return (
     <div>
-      
+      <Stack spacing={4} direction="row">
+        <Button variant="contained" color="primary">
+          Button
+        </Button>
+        <Button variant="contained" color="secondary">
+          Button
+        </Button>
+      </Stack>
     </div>
   );
 }
