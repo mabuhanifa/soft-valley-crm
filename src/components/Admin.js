9a237dfbbe8f08dfaddf4d87f860c9bf0fcaf920
shourovahmd <moh.abuhanifa@gmail.com>
2023-02-21 03:52:36 +0600

    login form

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import SideBar from "./SideBar";
 
 export default function Admin() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const logIn = (e) => {
+    e.preventDefault();
+  };
   return (
     <div className="home">
       <div className="side">
         <SideBar />
       </div>
-      <div>Admin</div>
+      <div className="adminContainer">
+        <div></div>
+        <div>
+          <form onSubmit={logIn}>
+            <input type="text" onChange={(e) => setEmail(e.target.value)} />
+            <input
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <input type="submit" value="Login" />
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
